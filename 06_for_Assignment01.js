3b78a5078ab006f196e0e6d5d98d3789fa39d256
Aj-Kamble <108751595+Aj-Kamble@users.noreply.github.com>
2024-03-23 15:22:33 +0530

    Assignment on for loop
    Assignment on for loop

---
+++ 06_for_Assignment01.js
@@ -0,0 +1,43 @@
+
+//WAP to print numbers from 5 to 15 by incrementing 1
+
+console.log(`Numbers from 5 to 15 by incrementing 1`);
+for(let i = 5; i<=15; i++){
+    console.log(i);
+}
+
+//WAP to print numbers from 50 to 40 by decrementing 1
+console.log(`Numbers from 50 to 40 by decrementing 1`);
+for(let i = 50; i>=40; i--){
+    console.log(i);
+}
+
+//WAP to find first 15 odd number
+console.log(`First 15 odd number`);
+for(let i = 0; i<=15; i = i+2){
+    console.log(i);
+}
+
+//WAP to find first 15 Even number
+console.log(`First 15 Even number`);
+for(let i = 1; i<=10; i = i+2){
+    console.log(i);
+}
+
+//WAP to print table of 5 like 5, 10, 15, 20....
+console.log(`table of 5 like 5`);
+for(let i = 5; i<=50; i = i+5){
+    console.log(i);
+}
+
+//WAP to print table of 10 like  10, 20....
+console.log(`table of 5 like 5`);
+for(let i = 10; i<=100; i = i+10){
+    console.log(i);
+}
+
+//WAP to print table of 10 reverse order like  100, 90, 80....
+console.log(`table of 10 reverse orde`);
+for(let i = 100; i>=10; i = i-10){
+    console.log(i);
+}
