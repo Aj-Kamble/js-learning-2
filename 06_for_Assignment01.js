f7e7082d648be1ac0ed6c4c3400d0f78c2242096
Aj-Kamble <108751595+Aj-Kamble@users.noreply.github.com>
2024-03-23 15:37:19 +0530

    forLoop - Assignment
    forLoop - Assignment

--- 06_for_Assignment01.js
+++ 06_for_Assignment01.js
@@ -25,19 +25,19 @@ for(let i = 1; i<=10; i = i+2){
 }
 
 //WAP to print table of 5 like 5, 10, 15, 20....
-console.log(`table of 5 like 5`);
+console.log(`table of 5`);
 for(let i = 5; i<=50; i = i+5){
     console.log(i);
 }
 
 //WAP to print table of 10 like  10, 20....
-console.log(`table of 5 like 5`);
+console.log(`table of 10`);
 for(let i = 10; i<=100; i = i+10){
     console.log(i);
 }
 
 //WAP to print table of 10 reverse order like  100, 90, 80....
-console.log(`table of 10 reverse orde`);
+console.log(`table of 10 reverse order`);
 for(let i = 100; i>=10; i = i-10){
     console.log(i);
 }
